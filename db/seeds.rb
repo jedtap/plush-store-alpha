2be8ed30701e98907f4996b9a49b023a980c4af7
Jed-Tapiador <doom.slaver2@gmail.com>
2022-08-22 22:24:22 +0800

    added a drill script

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,13 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+prods = Product.create([
+  { name: 'Bulbasaur Plushie', price: 14.99, alt: 'photo of a Bulbasaur plushie', img: '../../assets/images/bulbasaur.jpg' },
+  { name: 'Charmander Plushie', price: 14.99, alt: 'photo of a Charmander plushie', img: '../../assets/images/charmander.jpg' },
+  { name: 'Ditto as Dragonite Plushie', price: 11.99, alt: 'photo of a Ditto plushie transformed into Dragonite', img: '../../assets/images/ditto-dragonite.jpg' },
+  { name: 'Ditto as Gengar Plushie', price: 11.99, alt: 'photo of a Ditto plushie transformed into Gengar', img: '../../assets/images/ditto-gengar.jpg' },
+  { name: 'Ditto as Lapras Plushie', price: 11.99, alt: 'photo of a Ditto plushie transformed into Lapras', img: '../../assets/images/ditto-lapras.jpg' },
+  { name: 'Eevee Plushie', price: 19.99, alt: 'photo of an Eevee plushie', img: '../../assets/images/eevee.jpg' },
+  { name: 'Pikachu Kanazawa Plushie', price: 27.99, alt: 'photo of a Pikachu plushie wearing a Kanazawa dress', img: '../../assets/images/pikachu-kanazawa.jpg' },
+  { name: 'Pikachu Plushie', price: 24.99, alt: 'photo of a Pikachu plushie', img: '../../assets/images/pikachu.jpg' },
+  { name: 'Squirtle Plushie', price: 14.99, alt: 'photo of a Squirtle plushie', img: '../../assets/images/squirtle.jpg' },
+  { name: 'Snorlax Plushie', price: 12.99, alt: 'photo of a Snorlax plushie', img: '../../assets/images/snorlax.jpg' },
+  { name: 'Growlithe Plushie', price: 12.99, alt: 'photo of a Growlithe plushie', img: '../../assets/images/growlithe.jpg' },
+])
