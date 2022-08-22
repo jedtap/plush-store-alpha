c49bb291751a6f89759da363ccc9166e6b9508df
Jed-Tapiador <doom.slaver2@gmail.com>
2022-08-22 21:40:27 +0800

    setup template of react

---
+++ client/src/components/App.js
@@ -0,0 +1,12 @@
+import logo from '../images/logo.svg';
+import '../styles/App.css';
+import Hehe from './Hehe'
+
+function App() {
+  return (<>
+    <img src={logo} className="App-logo" alt="logo" />
+    <Hehe />
+  </>);
+}
+
+export default App;
