10171745127e59d3fef9586b605e55fb13df714e
Jed-Tapiador <doom.slaver2@gmail.com>
2022-08-27 09:36:38 +0800

    router and fallback controller edits

--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,6 @@ Rails.application.routes.draw do
 
   get 'last_buyer', to: 'buyers#last_buyer'
   root 'home#index'
-  match '*path', to: 'home#index', via: :all
+  # match '*path', to: 'home#index', via: :all
+  get "*path", to: "home#index", constraints: ->(req) { !req.xhr? && req.format.html? }
 end
